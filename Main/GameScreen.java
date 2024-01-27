93bf324360c5a93630fb4ca1b3f55a87c5ccb3c1
Elvin-Jiby <elvin.jiby@ucdconnect.ie>
2024-01-27 23:48:17 +0000

    Window opens (with title and yellow box on screen)

--- Main/GameScreen.java
+++ Main/GameScreen.java
@@ -8,12 +8,14 @@ public class GameScreen extends JPanel {
 
     public GameScreen(Game game) {
         this.game = game;
-        setPreferredSize(new Dimension(1280,720));
+        setPreferredSize(new Dimension(640,360));
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.setColor(Color.YELLOW);
+        g.fillRect(100,100,50,100);
     }
 
     public Game getGame() { return game; }
