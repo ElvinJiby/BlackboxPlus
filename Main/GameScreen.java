b4869fdea3c153586b182d9eb2ca2592f6a736cd
Elvin-Jiby <elvin.jiby@ucdconnect.ie>
2024-01-30 13:37:20 +0000

    added bg image of the board (small version) + testing with buttons

--- Main/GameScreen.java
+++ Main/GameScreen.java
@@ -1,10 +1,7 @@
 package Main;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
 
 public class GameScreen extends JPanel {
     private Game game;
@@ -13,25 +10,25 @@ public class GameScreen extends JPanel {
     public GameScreen(Game game) {
         this.game = game;
         setPreferredSize(new Dimension(1280,720)); // setting the resolution of the game
-        bgImage = getBGImage();
-    }
+        bgImage = (new ImageIcon("./res/blackboxbgimagesmallver.jpg")).getImage(); // loads the bg image for the game
 
-    private Image getBGImage() {
-        try {
-            bgImage = ImageIO.read(new File("./res/blackboxbgimage.jpg"));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally {
-            return bgImage;
-        }
+        JButton button = new JButton(); // creates a new button
+        button.setBounds(640, 100, 500,500); // sets location and size of the button
+        button.setText("This is a button"); // sets the text to be displayed on the button
+        button.setFocusable(false); // removes weird outline around text on button
+        button.setFont(new Font("Comic Sans", 0, 14)); // changes font
+        button.addActionListener(e -> { // the action to be performed once the button is clicked
+            System.out.println("Button was clicked");
+            button.setText("Button clicked.");
+            button.setEnabled(false); // disables the button once pressed
+        });
+        this.add(button); // adding the button to the screen
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(bgImage, 0, 0, null);
-//        g.setColor(Color.YELLOW); // sets the color of whatever graphic we're going to draw to Yellow
-//        g.fillRect(100,100,50,100); // draws a rectangle at the x,y coords with a width of 50 pixels and height of 100 pixels
     }
 
     public Game getGame() { return game; } // accessor
