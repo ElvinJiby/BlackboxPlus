28413efff54a0be4b3f1e9c011abf17b71a138ab
ayush014253 <126803975+ayush014253@users.noreply.github.com>
2024-02-16 09:37:52 +0000

    added classes for the board

---
+++ Main/array.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class array {
+    private node[][] bord=new node [3][3];
+    public node [][]getbord(){
+        return bord;
+    }
+
+}
---
+++ Main/node.java
@@ -0,0 +1,6 @@
+package Main;
+
+public class node {
+    private int value;
+    private  node[] next=new node [6];
+}
