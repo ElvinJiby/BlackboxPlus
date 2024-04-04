4d1dd2ba4880ffc84914dfe5d347a8b5d7322b40
shan-e <126803195+shan-e@users.noreply.github.com>
2024-04-04 02:15:12 +0100

    updated playname changing

--- main/GameScreen.java
+++ main/GameScreen.java
@@ -4,38 +4,57 @@ import inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class GameScreen extends JPanel {
     // Application Variables
     private Game game;
     private MouseInputs mouseInputs;
-    private JTextField textField;
+    private JTextField arrowNumberInputField;
+    private JTextField playerNameInputField;
     private JLabel resultLabel;
+    private JLabel playerNameLabel;
+    private JLabel scoreLabel;
+    private int score = 100;
     private int value = 1;
+    private String playerName = "user34567";
 
     public GameScreen(Game game) {
         this.game = game;
-        setPreferredSize(new Dimension(1280,720)); // setting the resolution of the game
+        setPreferredSize(new Dimension(1280, 720)); // setting the resolution of the game
         setBackground(Color.BLACK);
+
+        scoreLabel = new JLabel("Score: " + score);
+        scoreLabel.setForeground(Color.WHITE);
+//        scoreLabel.setBounds(1100, 650, 150, 30);
 
         mouseInputs = new MouseInputs(this); // add mouse input support
         addMouseListener(mouseInputs); // needed to perform mouse actions
 
-        textField = new JTextField(10);
-        textField.setText("1");
-        textField.setBounds(50,50,250,250);
-        textField.addActionListener(e -> validateInput());
+        arrowNumberInputField = new JTextField(10);
+        arrowNumberInputField.setText("1");
+//        textField.setBounds(50, 50, 250, 250);
+        arrowNumberInputField.addActionListener(e -> validateInput());
         resultLabel = new JLabel("Valid Integer: " + value);
         resultLabel.setForeground(Color.WHITE);
+
+        playerNameInputField = new JTextField(20);
+        playerNameInputField.setText("user34567");
+//        textField.setBounds(50, 50, 250, 250);
+        playerNameInputField.addActionListener(e -> updateUsername());
+
+        playerNameLabel = new JLabel("Player Name: " + playerName);
+        playerNameLabel.setForeground(Color.WHITE);
 
         JLabel label = new JLabel("Enter an integer between 1 and 59: ");
         label.setForeground(Color.WHITE);
 
+        add(playerNameInputField);
+        add(playerNameLabel);
         add(label);
-        add(textField);
+        add(arrowNumberInputField);
         add(resultLabel);
+        add(scoreLabel);
+
 
         JLabel colorWindow = new JLabel();
         colorWindow.setBackground(Color.WHITE);
@@ -61,8 +80,8 @@ public class GameScreen extends JPanel {
 
     private void addSpotButtons(GameScreen gameScreen) {
         int[][] buttonPosArray = new int[][]{
-                {425,39}, {603,18}, {665,25}, {842,39}, {911,172}, {958,219}, {1030,366}, {941,498}, {917,558},
-                {826,682}, {658,690}, {596,689}, {438,675}, {356,549}, {338,515}, {245,352}, {316,214}, {352,174},
+                {425, 39}, {603, 18}, {665, 25}, {842, 39}, {911, 172}, {958, 219}, {1030, 366}, {941, 498}, {917, 558},
+                {826, 682}, {658, 690}, {596, 689}, {438, 675}, {356, 549}, {338, 515}, {245, 352}, {316, 214}, {352, 174},
         };
 
         for (int i = 0; i < buttonPosArray.length; i++) {
@@ -73,30 +92,40 @@ public class GameScreen extends JPanel {
             button.setBounds(xPos, yPos, 50, 50);
             button.setFocusable(false);
 
-            int buttonNo = i+1;
+            int buttonNo = i + 1;
             button.setText(String.valueOf(buttonNo));
             gameScreen.add(button);
         }
     }
 
     @Override
-    public void paintComponent(Graphics g) { super.paintComponent(g); game.render(g); }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        game.render(g);
+    }
 
     private void validateInput() {
         try {
-            value = Integer.parseInt(textField.getText());
+            value = Integer.parseInt(arrowNumberInputField.getText());
             if (value < 1 || value > 59) {
                 JOptionPane.showMessageDialog(this, "Enter a number between 1 and 59", "Invalid Input", JOptionPane.ERROR_MESSAGE);
-                textField.setText("1"); // Reset to default value
-                resultLabel.setText("Valid Integer: ");
-            }
-            else resultLabel.setText("Valid Integer: " + value);
+                arrowNumberInputField.setText("1"); // Reset to default value
+                value = 1;
+                resultLabel.setText("Valid Integer: " + value);
+            } else resultLabel.setText("Valid Integer: " + value);
         } catch (NumberFormatException ex) {
             JOptionPane.showMessageDialog(this, "Please enter a valid integer.", "Invalid Input", JOptionPane.ERROR_MESSAGE);
-            textField.setText("1"); // Reset to default value
+            arrowNumberInputField.setText("1"); // Reset to default value
             resultLabel.setText("Valid Integer: ");
         }
     }
 
-    public Game getGame() { return game; } // accessor
+    private void updateUsername() {
+        playerName = playerNameInputField.getText();
+        playerNameLabel.setText(playerName);
+    }
+
+    public Game getGame() {
+        return game;
+    } // accessor
 }
