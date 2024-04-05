25076b3d9249aa5af15b5bb304dd7866f0438fe2
ayush014253 <126803975+ayush014253@users.noreply.github.com>
2024-04-05 07:37:30 +0100

    made a new class to access the board

--- computations/lists.java
+++ computations/lists.java
@@ -3,19 +3,19 @@ package computations;
 import java.util.ArrayList;
 
 public class lists {
-   public static void main(String[] args) {
-       board boardp=new board();
-       ArrayList<Integer> ints=new ArrayList<>();
-       node [] e=new node[54];
-       int a=5;
-       int t=0;
-       int c=0;
+    public board createboard(){
+        board boardp=new board();
+        ArrayList<Integer> ints=new ArrayList<>();
+        node [] e=new node[54];
+        int a=5;
+        int t=0;
+        int c=0;
         for(int i=0;i<9;i++){
-           for(int j=0;j<a;j++){
+            for(int j=0;j<a;j++){
                 c++;
-               node n=new node(c,6);
-               n.setexit(false);
-               n.setnext(7);
+                node n=new node(c,6);
+                n.setexit(false);
+                n.setnext(7);
                 boardp.setBoardindex(i,j,n);
 
 
@@ -34,12 +34,22 @@ public class lists {
             e[i].setValue(i+1);
             e[i].setexit(true);
         }
-            boardp.setexit(e);
-            System.out.println(boardp);
+        boardp.setexit(e);
+        System.out.println(boardp);
+
+
+        boardp.linkboard();
+        return boardp;
+    }
+   public static void main(String[] args) {
+       board boardp=new board();
+       ArrayList<Integer> ints=new ArrayList<>();
+       lists c=new lists();
+       boardp=c.createboard();
        boardp.getnode(4,3).setatom(true);
        boardp.getnode(4,2).setatom(true);
 
-        boardp.linkboard();
+
 
         ints= boardp.iterate(17);
 
