b85628dd65532e0b73f899793769859880578123
shan-e <126803195+shan-e@users.noreply.github.com>
2024-04-09 16:39:46 +0100

    os check

--- src/java/src/main/Main.java
+++ src/java/src/main/Main.java
@@ -6,6 +6,8 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        String os  = Main.getOperatingSystem();
+        System.out.println(os);
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -19,4 +21,10 @@ public class Main {
             }
         });
     }
+    public static String getOperatingSystem() {
+        String os = System.getProperty("os.name");
+        // System.out.println("Using System Property: " + os);
+        return os;
+    }
 }
+
