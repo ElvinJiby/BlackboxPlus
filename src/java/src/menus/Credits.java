2995178225b21cb74244390df113cb840ff26ed9
shan-e <126803195+shan-e@users.noreply.github.com>
2024-04-16 13:54:04 +0100

    cleaned code and introduced instructions to exit window

--- src/java/src/menus/Credits.java
+++ src/java/src/menus/Credits.java
@@ -3,36 +3,35 @@ package menus;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Objects;
 
-public class Credits implements Runnable{
-    private static JFrame jFrame;
+public class Credits implements Runnable {
+    private static JFrame jFrame; // credits window
+    private static ImageIcon creditsGIF; // GIF image
+    private static JLabel creditsJLabel; // Image cast as JLabel
+
+    @Override public void run() { jFrame.setVisible(true); } // overriding run method to set JFrame visibility to true
 
     public Credits() {
-        jFrame = new JFrame("Credits");
-        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        jFrame.setResizable(false);
-        jFrame.setSize(1280, 720);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setIconImage(new ImageIcon(getClass().getResource("/Icons/new_icon.png")).getImage());
+        jFrame = new JFrame("Credits"); // creates new JFrame object with "Credits" as title
+        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // if window is closed, program does not terminate
+        jFrame.setResizable(false); // restricts resizing
+        jFrame.setSize(1280, 720); // 720p window
+        jFrame.setLocationRelativeTo(null); // centre window according to device screen size
+        jFrame.setIconImage(new ImageIcon(Objects.requireNonNull(getClass().getResource("/Icons/new_icon.png"))).getImage()); // sets icon image
 
-        ImageIcon creditsGIF = new ImageIcon(getClass().getResource("/Miscellaneous/credits_720p.gif"));
-        JLabel creditsJLabel = new JLabel(creditsGIF);
-        creditsJLabel.setSize(1280, 720);
+        creditsGIF = new ImageIcon(Objects.requireNonNull(getClass().getResource("/Miscellaneous/credits_720p.gif"))); // open gif image
+        creditsJLabel = new JLabel(creditsGIF); // save image as JLabel
+        creditsJLabel.setSize(1280, 720); // set size to 720p
+        jFrame.add(creditsJLabel); // add JLabel to JFrame
 
-        jFrame.add(creditsJLabel);
-
-//        jFrame.addKeyListener(new KeyListener() {
-//            @Override public void keyTyped(KeyEvent e) {}
-//            @Override public void keyReleased(KeyEvent e) {}
-//            @Override public void keyPressed(KeyEvent e) {
-//                if (e.getKeyChar() == 'q' || e.getKeyChar() == 'Q') System.exit(0);
-//                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) System.exit(0);
-//                if (e.getKeyChar() == 'R' || e.getKeyChar() == 'r') {
-//                    jFrame.dispose();
-//                    new StartScreen();
-//                }
-//            }});
+        // code to check if key is pressed to exit
+        jFrame.addKeyListener(new KeyListener() {
+            @Override public void keyTyped(KeyEvent e) {} // checks key typed
+            @Override public void keyReleased(KeyEvent e) {} // checks key released
+            @Override public void keyPressed(KeyEvent e) { // if q/Q/ESC is pressed, go back to start screen
+                if (e.getKeyChar() == 'q' || e.getKeyChar() == 'Q') { jFrame.dispose(); new StartScreen(); }
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) { jFrame.dispose(); new StartScreen(); }
+            }});
     }
-    @Override
-    public void run() {jFrame.setVisible(true);}
 }
