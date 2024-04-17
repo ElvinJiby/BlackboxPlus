1cd377e0424115443cc193425abde1cb62f41f2f
shan-e <126803195+shan-e@users.noreply.github.com>
2024-04-17 18:26:44 +0100

    added comments and cleaned code

--- src/java/src/inputs/OperatingSystem.java
+++ src/java/src/inputs/OperatingSystem.java
@@ -2,6 +2,6 @@ package inputs;
 
 public class OperatingSystem {
     public static boolean isItMacOS() {
-        return System.getProperty("os.name").equals("MacOS X"); // if name is macOS, return true
+        return System.getProperty("os.name").equals("Mac OS X"); // if name is macOS, return true
     }
 }
