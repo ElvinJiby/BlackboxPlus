7bca62f1dffb4540c04a3d092fe532b712134164
shan-e <126803195+shan-e@users.noreply.github.com>
2024-05-05 23:31:43 +0100

    clean code practise

--- src/java/src/computations/LeaderBoardData.java
+++ src/java/src/computations/LeaderBoardData.java
@@ -7,7 +7,6 @@ public class LeaderBoardData {
     // linked hash map ensures there is order when writing to the txt file and map list holds the name and score
     private static final LinkedHashMap<String, Integer> scoresLinkedHashMap = new LinkedHashMap<>();
     private static List<Map.Entry<String, Integer>> sortedScores = new ArrayList<>();
-    // getter methods
     public LinkedHashMap<String, Integer> getScoresLinkedHashMap() {
         return scoresLinkedHashMap;
     }
@@ -16,50 +15,43 @@ public class LeaderBoardData {
     }
 
     public void readAndSort() throws IOException {
-        LeaderBoardData leaderBoardData = new LeaderBoardData(); // create new instance
-        leaderBoardData.readTXTFile(); // read text file
-        sortedScores = leaderBoardData.sortScores(); // sort scores and save to map
+        LeaderBoardData leaderBoardData = new LeaderBoardData();
+        leaderBoardData.readTXTFile();
+        sortedScores = leaderBoardData.sortScores();
     }
 
     public void readTXTFile() throws IOException { // reads in txt file and saves it to the linked hash map
-        File file = new File("scores.txt"); // open file
-        if (!file.exists()) file.createNewFile(); // make new file if not there
-        Scanner scanner = new Scanner(file); // scanner takes in file object
-        while (scanner.hasNextLine()) {// keep reading in each string line until the end of the file
-            String line = scanner.nextLine(); // save each line in the line variable
-            String[] column = line.split(","); // divide this line by comma to get name and score
+        File file = new File("scores.txt");
+        if (!file.exists()) file.createNewFile();
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] column = line.split(",");
             String name = column[0]; // name will always be the 1st column
             int score = Integer.parseInt(column[1]); // score will always be the 2nd column parsed
-            scoresLinkedHashMap.put(name, score); // add this name and score to the linked hash map
+            scoresLinkedHashMap.put(name, score);
         }
     }
 
     // I used lists, as they maintain order as they're inserted.
     // lists allow for sorting based on the integer part of the linked-hashmap
-    public List<Map.Entry<String, Integer>> sortScores() {// sorts LinkedHashMap by returning a List
-        List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<>(scoresLinkedHashMap.entrySet()); // Map.Entry ensures key-value pairs are still in the map
-        // x and y are the 2 arguments that need to be compared
-        sortedEntries.sort(Comparator.comparingInt(Map.Entry::getValue)); // getValue gets access to argument value and compares y to x to allow for descending order
-        return sortedEntries; // return the list as a sorted list
+    public List<Map.Entry<String, Integer>> sortScores() {
+        List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<>(scoresLinkedHashMap.entrySet());
+        sortedEntries.sort(Comparator.comparingInt(Map.Entry::getValue));
+        return sortedEntries;
     }
 
-    // this sorted list is then over-written to the txt file
-    public void writeTXTFile(List<Map.Entry<String, Integer>> sortedEntry) {// takes in the sorted list as a map
-        // FileWriter opens the file and PrintWriter writes to the file
+    public void writeTXTFile(List<Map.Entry<String, Integer>> sortedEntry) {
         try (PrintWriter printWriter = new PrintWriter(new FileWriter("scores.txt"))) {
-            // the loop goes through each entry in the list
-            // it accesses the name and score using entry.getKey and entry.getValue
-            // using PrintWriter, it writes to the file, seperated by a comma, for formatting
             for (Map.Entry<String, Integer> entry : sortedEntry) printWriter.println(entry.getKey() + "," + entry.getValue());
         }
         catch (IOException e) { System.out.println("Error writing the file!"); }
     }
 
-    // write the scores to txt file
     public static void storeScore(String name, int score) {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("scores.txt", true))) { // append score to file
-            bufferedWriter.write(name + "," + score); // write name and score seperated by commas
-            bufferedWriter.newLine(); // \n
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("scores.txt", true))) {
+            bufferedWriter.write(name + "," + score);
+            bufferedWriter.newLine();
         }
         catch (IOException e) {
             System.err.println("Error storing score: " + e.getMessage());
@@ -68,26 +60,23 @@ public class LeaderBoardData {
     }
 
     public static void checkFormat() throws IOException {
-        File file = new File("scores.txt"); // new file object
-        if (!file.exists()) file.createNewFile(); // make new file if not there
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("scores.txt")); // reader object
+        File file = new File("scores.txt");
+        if (!file.exists()) file.createNewFile();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("scores.txt"));
         int numOfLines = 0;
         String line;
-        // check if the line is not empty
-        while ((line = bufferedReader.readLine()) != null) if (!line.isEmpty()) numOfLines++; // increment numOfLines until EOF
-        bufferedReader.close(); // close object
-        // if there are not 5 players, label has to be default values
+        while ((line = bufferedReader.readLine()) != null) if (!line.isEmpty()) numOfLines++;
+        bufferedReader.close();
         if (numOfLines < 5) {
-            FileWriter fileWriter = new FileWriter("scores.txt", false); // do not append
-            fileWriter.write("user1,100\nuser2,100\nuser3,100\nuser4,100\nuser5,100\n"); // defaults
+            FileWriter fileWriter = new FileWriter("scores.txt", false);
+            fileWriter.write("user1,100\nuser2,100\nuser3,100\nuser4,100\nuser5,100\n");
             fileWriter.close();
         }
     }
 
     public static void clearTheLeaderboard() throws IOException {
         sortedScores.clear();
-        FileWriter fileWriter = new FileWriter("scores.txt", false); // do not append
-        // fill with default values
+        FileWriter fileWriter = new FileWriter("scores.txt", false);
         fileWriter.write("user1,100\nuser2,100\nuser3,100\nuser4,100\nuser5,100\n");
         fileWriter.close();
     }
--- src/java/src/computations/Lists.java
+++ src/java/src/computations/Lists.java
@@ -2,10 +2,10 @@ package computations;
 
 public class Lists {
     public Board createboard() {//creates a board for the game
-        Board boardp = new Board();
+        Board board = new Board();
         Node[] e = new Node[54];
         int a = 5;//set to 5 for the first set of horizontal hexagons and increases until 9 when it reaches to the middle then starts to decrease
-        int t = 0;//used as a bolean value which specifies if an increases or decreases
+        int t = 0;//used as a boolean value which specifies if an increases or decreases
         int c = 0;//used for each hexagonal nodes value
         for (int i = 0; i < 9; i++) {//iterates to create 9 rows
             for (int j = 0; j < a; j++) {//iterates to create 5 - 9 columns and then again to 9-5 columns
@@ -13,7 +13,7 @@ public class Lists {
                 Node n = new Node(c, 6);//creates a hexagonal box with 6 sides
                 n.setexit(false);
                 n.setnext(7);
-                boardp.setBoardindex(i, j, n);//puts node n in position (i,j)in the 2d array
+                board.setBoardindex(i, j, n);//puts node n in position (i,j)in the 2d array
 
 
             }
@@ -30,11 +30,10 @@ public class Lists {
             e[i].setNodeValue(i + 1);
             e[i].setexit(true);//this helps us know that this node is an exit node
         }
-        boardp.setexit(e);
-        // System.out.println(boardp);
+        board.setexit(e);
 
 
-        boardp.linkboard();//calls the linkboard function so all the boards are linked to eachother
-        return boardp;
+        board.linkboard();//calls the link board function so all the boards are linked to each-other
+        return board;
     }
 }
